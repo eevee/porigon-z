9e3f0f919b4681c4580fc1047899dd8d562bc13e
Eevee <git@veekun.com>
2009-09-08 14:13:10 -0700

    Sprite decryption now uses a generator for the mask.
    a_magical_me is awesome for providing some enlightenment on why the game
    acts as it does and reminding me of how to reverse the mask generation
    so I can start from the upper left pixel.
    
    This fixes decryption of a handful of large Pokémon's sprites in
    Platinum.

--- porigonz/nds/util/sprites.py
+++ porigonz/nds/util/sprites.py
@@ -1,35 +1,6 @@
 # encoding: utf8
 """Handling NDS sprites."""
 
-"""
-(01:50:06 AM) Zhen Lin: well, if you want to do it again, I have the seed values for the encryption used in Platinum
-(01:51:02 AM) Eevee: I don't think I ever implemented the real formula, since you beat me to finishing it
-(01:51:11 AM) Zhen Lin: Ah
-(01:51:16 AM) Zhen Lin: well, it's fairly simple
-(01:51:47 AM) Zhen Lin: pika found out a few weeks/months later what it really was
-(01:52:09 AM) Zhen Lin: the seed value for each file is the final 16-bit integer (call it q[3199])
-(01:52:49 AM) Zhen Lin: We construct a mask m[i], i = 0 to 3199
-(01:52:54 AM) Zhen Lin: m[3199] = q[3199]
-(01:53:05 AM) Zhen Lin: m[i - 1] = m[i] * r + k
-(01:53:11 AM) Zhen Lin: for particular constants r and k
-(01:53:28 AM) Eevee: and then, what, xor it all?
-(01:53:35 AM) Zhen Lin: yeah
-(01:53:47 AM) Zhen Lin: decrypted value = q[i] XOR m[i]
-(01:53:56 AM) Eevee: wow.  that is embarrassingly simple
-(01:54:09 AM) Eevee: are r/k just arbitrary game-global constants?
-(01:54:12 AM) Zhen Lin: yeah
-(01:54:20 AM) Zhen Lin: same constants for trainer images, if I remember
-(01:54:46 AM) Zhen Lin: For DP, r = 0x4E6D, k = 0x6073
-(01:54:58 AM) Zhen Lin: for Platinum, r = 0xEB65, k = 0x61A1
-(01:55:09 AM) Eevee: wonder why they changed them
-(01:55:14 AM) Eevee: maybe they're based on something else
-(01:55:19 AM) Zhen Lin: Maybe
-(01:55:31 AM) Zhen Lin: Or maybe they wanted to annoy us for a few hours while we tried to find it
-(01:55:50 AM) Eevee: haha they could have done much better if the goal was to personally irritate a handful of people
-(01:55:56 AM) Zhen Lin: indeed
-(02:06:24 AM) Zhen Lin: The algorithm is actually a simple pseudorandom number generator, and there are certain requirements for the choice of constants to be good. If you're interested: http://en.wikipedia.org/wiki/Linear_congruential_generator
-"""
-
 from collections import namedtuple
 from cStringIO import StringIO
 
@@ -128,33 +99,6 @@ rahc_struct = Struct('rahc',
     MetaField('data', lambda ctx: 6400),  # XXX length is off by a factor of 1024??
 )
 
-# Thanks, Wikipedia!  Now I don't have to write these!
-# These compute the modular multiplicative inverse of a mod m, which is used to
-# construct the decryption mask for a Pokémon sprite.  They can also,
-# hopefully, be used to break the cipher's constants in the first place.
-def extended_gcd(a, b):
-    x, last_x = 0, 1
-    y, last_y = 1, 0
- 
-    while b:
-        quotient = a // b
-        a, b = b, a % b
-        x, last_x = last_x - quotient*x, x
-        y, last_y = last_y - quotient*y, y
- 
-    return (last_x, last_y, a)
- 
- 
-def inverse_mod(a, m):
-    x, q, gcd = extended_gcd(a, m)
- 
-    if gcd == 1:
-        # x is the inverse, but we want to be sure a positive number is returned.
-        return (x + m) % m
-    else:
-        # if gcd != 1 then a and m are not coprime and the inverse does not exist.
-        return None
-
 Size = namedtuple('Size', ['width', 'height'])
 
 class Sprite(object):
@@ -166,6 +110,8 @@ class Sprite(object):
 
         This encryption is only used for the Pokémon themselves and trainers.
         Items, berries, the map, the bag, etc. are all regular DS sprites.
+
+        Encryption is an affine cipher, apparently based on the Pokémon PRNG.
         """
 
         self = cls()
@@ -173,29 +119,36 @@ class Sprite(object):
         rgcn = rgcn_struct.parse(chunk)
         rahc = rahc_struct.parse(rgcn.data)
 
-        # XXX make these less constant sometime
+        # XXX make these less constant sometime.
+        # XXX and determine which constants to use.
         self.size = Size(width=160, height=80)
-        #add = 0x89c3  # appears to be the first dummy block only
+        # D/P
+        # similarly uses a different add constant for the first block
         add = 0x61a1
         mult = 0xeb65
-        # D/P
-        #add = 0xedf9
-        #mult = 0x4e6d
+        # Platinum
+        # These actually produce the same sequence as D/P, only backwards.  It
+        # appears the mask in D/P started at the end and was generated
+        # backwards, but some sprites in Platinum have a non-blank last pixel,
+        # so they had to switch it around and use the first pixel (with the
+        # same constants) as a seed instead.  I'm always using the first pixel
+        # as the seed, so I have inverse constants
+        # add = 0x89c3  # appears to be the first dummy block only?
+        add = 0x6073
+        mult = 0x4e6d
 
-        # Construct the decryption mask.  It starts with the last uint16 in the
-        # data and is modified going backwards.
-        # XXX use a generator for this
-        # XXX surely there's some way to make this forwards
-        mask = [0] * 3200
-        # cheap way to get the last word (ha ha)
-        mask[-1] = next(word_iterator(rahc.data[-2:], 16))
-        for i in reversed(range(len(mask) - 1)):
-            mask[i] = cap_to_bits(mask[i + 1] * mult + add, 16)
+        # Simple mask generator
+        def mask_generator():
+            key = next(word_iterator(rahc.data[0:2], 16))
+            while True:
+                yield key
+                key = cap_to_bits(key * mult + add, 16)
 
         # Unmask the sprite data
+        mask = mask_generator()
         self.pixels = [[0] * self.size.height for _ in range(self.size.width)]
         for i, word in enumerate(word_iterator(rahc.data, 16)):
-            unmasked = word ^ mask[i]
+            unmasked = word ^ next(mask)
 
             # This is 16 bits, and there are four bits per pixel, so we have
             # four pixels
